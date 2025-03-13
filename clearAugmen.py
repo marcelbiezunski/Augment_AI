36af10bbd707b64a2167490dbfe3b55d18a4aacd
marcel_biezunski <marcel200210@gmail.com>
2025-03-13 20:10:13 +0100

    Update clearAugmen.py

--- clearAugmen.py
+++ clearAugmen.py
@@ -1,23 +1,19 @@
 import os
 
-def usun_zdjecia_z_podkresleniem(katalog):
-    """Usuwa zdjęcia zawierające znak podkreślenia z danego katalogu.
-
-    Args:
-        katalog: Ścieżka do katalogu, w którym mają być usunięte zdjęcia.
-    """
-
-    for plik in os.listdir(katalog):
-        if "_" in plik:  # Sprawdzenie, czy nazwa pliku zawiera znak podkreślenia
-            sciezka_pliku = os.path.join(katalog, plik)
+def delete_data(catalog):
+    for file in os.listdir(catalog):
+        if "_" in file:
+            file_directory = os.path.join(catalog, file)
             try:
-                os.remove(sciezka_pliku)
-                print(f"Usunięto plik: {sciezka_pliku}")
+                os.remove(file_directory)
+                print(f"File deleted: {file_directory}")
             except OSError as e:
-                print(f"Błąd podczas usuwania pliku {sciezka_pliku}: {e}")
+                print(f"File removal failed: {file_directory}: {e}")
 
 if __name__ == "__main__":
-    katalog = "PetImages/train/Cat"
-    usun_zdjecia_z_podkresleniem(katalog)
-    katalog = "PetImages/train/Dog"
-    usun_zdjecia_z_podkresleniem(katalog)
+    catalog = "PetImages/train/Cat"
+    delete_data(catalog)
+    catalog = "PetImages/train/Dog"
+    delete_data(catalog)
+
+# catalog = location where augmented files are stored
