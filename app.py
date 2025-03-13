434cf9e703cf7426d8ef1263d86f99860c70d201
marcel_biezunski <marcel200210@gmail.com>
2025-03-13 20:45:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,21 +4,6 @@ import cv2
 import sys
 import random
 
-
-def remove_unreadable_image(img_path):
-  """Removes an image file if it cannot be read by OpenCV.
-
-  Args:
-    img_path: Path to the image file.
-  """
-
-  image = cv2.imread(img_path)
-  if image is None:
-    # Handle loading error (e.g., print message, skip image)
-    print(f"Error loading image: {img_path}")
-    os.remove(img_path)
-    print(f"Usunięto plik: {img_path}")
-
 from augmenLib import (
     random_crop,
     random_rotate,
@@ -29,7 +14,8 @@ from augmenLib import (
     add_gaussian_noise,
     add_salt_and_pepper_noise_color,
     random_perspective_points,
-    perspective_transform    
+    perspective_transform,
+    autoencoder    
 )
 
 
@@ -42,8 +28,7 @@ def get_image_names(folder):
 
 
 def main():
-    cats_names = get_image_names("PetImages/train/Cat")
-    dogs_names = get_image_names("PetImages/train/Dog")
+    image_names = get_image_names("photos")
 
     print("percentage of images to augment: ")
     print("[0] 25%")
@@ -55,17 +40,13 @@ def main():
     percentage = int(input("enter here: "))
     match percentage:
         case 0:
-            num_images_cats = int(len(cats_names) * 0.25)
-            num_images_dogs = int(len(dogs_names) * 0.25)
+            num_images = int(len(image_names) * 0.25)
         case 1:
-            num_images_cats = int(len(cats_names) * 0.5)
-            num_images_dogs = int(len(dogs_names) * 0.5)
+            num_images = int(len(image_names) * 0.5)
         case 2:
-            num_images_cats = int(len(cats_names) * 0.75)
-            num_images_dogs = int(len(dogs_names) * 0.75)
+            num_images = int(len(image_names) * 0.75)
         case 3:
-            num_images_cats = len(cats_names)
-            num_images_dogs = len(dogs_names)
+            num_images = len(image_names)
         case 4:
             sys.exit("Have a nice day!")
 
@@ -80,115 +61,87 @@ def main():
     print("[6] add_gaussian_noise ")
     print("[7] add_salt_and_pepper_noise_color ")
     print("[8] perspective_transform ")
-    print("[9] exit application")
+    print("[9] autoencoder ")
+    print("[10] exit application")
 
     choice = int(input("enter here: "))
 
     match choice:
         case 0:
-            cats_names_random = random.sample(cats_names, num_images_cats)
-            for image_path in cats_names_random:
-                #remove_unreadable_image("PetImages/train/Cat/" + image_path)
-                new_image = random_crop("PetImages/train/Cat/" + image_path)
-                cv2.imwrite(f"PetImages/train/Cat/{image_path}_cropped.jpg", new_image)
-            
-            dogs_names_random = random.sample(dogs_names, num_images_dogs)
-            for image_path in dogs_names_random:
-                #remove_unreadable_image("PetImages/train/Dog/" + image_path)
-                new_image = random_crop("PetImages/train/Dog/" + image_path)
-                cv2.imwrite(f"PetImages/train/Dog/{image_path}_cropped.jpg", new_image)
+            print("WARNING: This method is not recommended for medical images, where every pixel is important")
+            num_images_random = random.sample(image_names,num_images)
+            for image_path in num_images_random:
+                new_image = random_crop("photos/" + image_path)
+                cv2.imwrite(f"photos/{image_path[:-4]}_cropped.jpg", new_image)
         
         case 1:
-            cats_names_random = random.sample(cats_names, num_images_cats)
-            for image_path in cats_names_random:
-                new_image = random_rotate("PetImages/train/Cat/" + image_path)
-                cv2.imwrite(f"PetImages/train/Cat/{image_path}_rotated.jpg", new_image)
-            
-            dogs_names_random = random.sample(dogs_names, num_images_dogs)
-            for image_path in dogs_names_random:
-                new_image = random_rotate("PetImages/train/Dog/" + image_path)
-                cv2.imwrite(f"PetImages/train/Dog/{image_path}_rotated.jpg", new_image)
+            print("WARNING: This method is not recommended for images with clear orientation, e.g. text image")
+            num_images_random = random.sample(image_names,num_images)
+            for image_path in num_images_random:
+                new_image = random_rotate("photos/" + image_path)
+                cv2.imwrite(f"photos/{image_path[:-4]}_rotated.jpg", new_image)
 
         case 2:
-            cats_names_random = random.sample(cats_names, num_images_cats)
-            for image_path in cats_names_random:
-                new_image = flip_image("PetImages/train/Cat/" + image_path)
-                cv2.imwrite(f"PetImages/train/Cat/{image_path}_flipped.jpg", new_image)
-            
-            dogs_names_random = random.sample(dogs_names, num_images_dogs)
-            for image_path in dogs_names_random:
-                new_image = flip_image("PetImages/train/Dog/" + image_path)
-                cv2.imwrite(f"PetImages/train/Dog/{image_path}_flipped.jpg", new_image)
+            print("WARNING: This method is not recommended for images with clear orientation, e.g. car image")
+            num_images_random = random.sample(image_names,num_images)
+            for image_path in num_images_random:
+                new_image = flip_image("photos/" + image_path)
+                cv2.imwrite(f"photos/{image_path[:-4]}_flipped.jpg", new_image)
         
         case 3:
-            cats_names_random = random.sample(cats_names, num_images_cats)
-            for image_path in cats_names_random:
-                new_image = random_resize("PetImages/train/Cat/" + image_path)
-                cv2.imwrite(f"PetImages/train/Cat/{image_path}_resised.jpg", new_image)
-            
-            dogs_names_random = random.sample(dogs_names, num_images_dogs)
-            for image_path in dogs_names_random:
-                new_image = random_resize("PetImages/train/Dog/" + image_path)
-                cv2.imwrite(f"PetImages/train/Dog/{image_path}_resised.jpg", new_image)
+            print("WARNING: This method is not recommended for images where size matter, e.g. medical image")
+            num_images_random = random.sample(image_names,num_images)
+            for image_path in num_images_random:
+                new_image = random_resize("photos/" + image_path)
+                cv2.imwrite(f"photos/{image_path[:-4]}_resised.jpg", new_image)
 
         case 4:
-            cats_names_random = random.sample(cats_names, num_images_cats)
-            for image_path in cats_names_random:
-                new_image = random_brightness("PetImages/train/Cat/" + image_path)
-                cv2.imwrite(f"PetImages/train/Cat/{image_path}_brightened.jpg", new_image)
-            
-            dogs_names_random = random.sample(dogs_names, num_images_dogs)
-            for image_path in dogs_names_random:
-                new_image = random_brightness("PetImages/train/Dog/" + image_path)
-                cv2.imwrite(f"PetImages/train/Dog/{image_path}_brightened.jpg", new_image)
+            print("WARNING: This method is not recommended for images where contrast matter, e.g. X-ray image")
+            num_images_random = random.sample(image_names,num_images)
+            for image_path in num_images_random:
+                new_image = random_brightness("photos/" + image_path)
+                cv2.imwrite(f"photos/{image_path[:-4]}_brightened.jpg", new_image)
 
         case 5:
-            cats_names_random = random.sample(cats_names, num_images_cats)
-            for image_path in cats_names_random:
-                new_image = adjust_contrast("PetImages/train/Cat/" + image_path)
-                cv2.imwrite(f"PetImages/train/Cat/{image_path}_adjust_contrast.jpg", new_image)
-            
-            dogs_names_random = random.sample(dogs_names, num_images_dogs)
-            for image_path in dogs_names_random:
-                new_image = adjust_contrast("PetImages/train/Dog/" + image_path)
-                cv2.imwrite(f"PetImages/train/Dog/{image_path}_adjust_contrast.jpg", new_image)
+            print("WARNING: This method is not recommended for images where contrast matter, e.g. X-ray image")
+            num_images_random = random.sample(image_names,num_images)
+            for image_path in num_images_random:
+                new_image = adjust_contrast("photos/" + image_path)
+                cv2.imwrite(f"photos/{image_path[:-4]}_adjust_contrast.jpg", new_image)
 
         case 6:
-            cats_names_random = random.sample(cats_names, num_images_cats)
-            for image_path in cats_names_random:
-                new_image = add_gaussian_noise("PetImages/train/Cat/" + image_path)
-                cv2.imwrite(f"PetImages/train/Cat/{image_path}_gaussian.jpg", new_image)
-            
-            dogs_names_random = random.sample(dogs_names, num_images_dogs)
-            for image_path in dogs_names_random:
-                new_image = add_gaussian_noise("PetImages/train/Dog/" + image_path)
-                cv2.imwrite(f"PetImages/train/Dog/{image_path}_gaussian.jpg", new_image)
+            print("WARNING: This method is not recommended for images where noise may lead to misdiagnosis, e.g. medical image")
+            num_images_random = random.sample(image_names,num_images)
+            for image_path in num_images_random:
+                new_image = add_gaussian_noise("photos/" + image_path)
+                cv2.imwrite(f"photos/{image_path[:-4]}_gaussian.jpg", new_image)
 
         case 7:
-            cats_names_random = random.sample(cats_names, num_images_cats)
-            for image_path in cats_names_random:
-                new_image = add_salt_and_pepper_noise_color("PetImages/train/Cat/" + image_path)
-                cv2.imwrite(f"PetImages/train/Cat/{image_path}_salt_and_pepper.jpg", new_image)
-            
-            dogs_names_random = random.sample(dogs_names, num_images_dogs)
-            for image_path in dogs_names_random:
-                new_image = add_salt_and_pepper_noise_color("PetImages/train/Dog/" + image_path)
-                cv2.imwrite(f"PetImages/train/Dog/{image_path}_salt_and_pepper.jpg", new_image)
+            print("WARNING: This method is not recommended for images where noise may lead to misdiagnosis, e.g. medical image")
+            num_images_random = random.sample(image_names,num_images)
+            for image_path in num_images_random:
+                new_image = add_salt_and_pepper_noise_color("photos/" + image_path)
+                cv2.imwrite(f"photos/{image_path[:-4]}_salt_and_pepper.jpg", new_image)
 
         case 8:
-            cats_names_random = random.sample(cats_names, num_images_cats)
-            for image_path in cats_names_random:
-                points = random_perspective_points("PetImages/train/Cat/" + image_path)
-                new_image = perspective_transform("PetImages/train/Cat/" + image_path, points)
-                cv2.imwrite(f"PetImages/train/Cat/{image_path}_perspective_transform.jpg", new_image)
+            print("WARNING: This method is not recommended for images where perspective is key, e.g. architecture image")
+            num_images_random = random.sample(image_names,num_images)
+            for image_path in num_images_random:
+                points = random_perspective_points("photos/" + image_path)
+                new_image = perspective_transform("photos/" + image_path, points)
+                cv2.imwrite(f"photos/{image_path[:-4]}_perspective_transform.jpg", new_image)
 
-            dogs_names_random = random.sample(dogs_names, num_images_dogs)
-            for image_path in dogs_names_random:
-                points = random_perspective_points("PetImages/train/Dog/" + image_path)
-                new_image = perspective_transform("PetImages/train/Dog/" + image_path, points)
-                cv2.imwrite(f"PetImages/train/Dog/{image_path}_perspective_transform.jpg", new_image)
-        
         case 9:
+            try:
+                print("WARNING: This method is time consuming, be patient")
+                img_path = "photos/"
+                autoencoder(img_path,num_images)
+            except ValueError as e:
+                print(f"Error: {e}. Please make sure that the images are placed in a separate folder and that the folder is copied to the 'photos' directory.")
+            except Exception as e:
+                print(f"Unexpected error: {e}")
+        
+        case 10:
             sys.exit("Have a nice day!")
 
     
